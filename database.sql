b3d82ebe1015a13d24d2a0f2e1214aa6c9d80d72
Maria Chikhireva <mchikhireva@gmail.com>
2025-01-28 22:25:00 +0300

    fix database

--- database.sql
+++ database.sql
@@ -1,14 +1,12 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS checks;
 
 
 CREATE TABLE urls (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE,
-    created_at TIMESTAMP
+    created_at DATE
 );
-
-
-DROP TABLE IF EXISTS checks:
 
 
 CREATE TABLE checks (
@@ -18,4 +16,4 @@ CREATE TABLE checks (
     h1 VARCHAR(255),
     title VARCHAR(255),
     description VARCHAR(255),
-    created_at TIMESTAMP);
+    created_at DATE);
