1627fbc4f727c7037d7acf17555f40abb1232bd3
Maria Chikhireva <mchikhireva@gmail.com>
2025-02-19 21:43:14 +0300

    add blueprints and context manager

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,89 +1,13 @@
-import requests
-from flask import (
-    Flask,
-    abort,
-    flash,
-    redirect,
-    render_template,
-    request,
-    url_for,
-)
-
-from page_analyzer import db_manager as db
+from flask import Flask
 from page_analyzer.settings import DATABASE_URL, SECRET_KEY
-from page_analyzer.utils import is_valid, normalize_url
+from page_analyzer.blueprints.main import main_bp
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['DATABASE_URL'] = DATABASE_URL
 
-
-@app.route('/')
-def index():
-    return render_template(
-        'index.html',
-    )
-
-
-@app.route('/urls', methods=['POST'])
-def add_url():
-    url = request.form.to_dict()['url']
-
-    if not is_valid(url):
-        flash('Некорректный URL', 'danger')
-        return render_template(
-            'index.html',
-            url=url,
-        ), 422
-    
-    normalized_url = normalize_url(url)
-    conn = db.connect_db(app)
-    existed_url = db.get_url_by_name(conn, normalized_url)
-    if existed_url:
-        flash('Страница уже существует', 'warning')
-        url_id = existed_url.id
-    else:
-        url_id = db.insert_url(conn, normalized_url)
-        flash('Страница успешно добавлена', 'success')
-    db.close_connection(conn)
-    return redirect(url_for('show_url', id=url_id))
-
-
-@app.route('/urls/<int:id>', methods=['GET'])
-def show_url(id: int):
-    conn = db.connect_db(app)
-    url = db.get_url_by_id(conn, id)
-    checks = db.get_checks(conn, id)
-    if not url:
-        abort(404)
-    db.close_connection(conn)
-    return render_template('url.html', url=url, checks=checks)
-
-
-@app.route('/urls', methods=['GET'])
-def show_urls():
-    conn = db.connect_db(app)
-    urls = db.get_urls(conn)
-    db.close_connection(conn)
-    return render_template('urls.html', urls=urls)
-
-
-@app.route('/urls/<int:id>/checks', methods=['POST'])
-def check_url(id: int):
-    conn = db.connect_db(app)
-    url = db.get_url_by_id(conn, id)
-    try:
-        response = requests.get(url.name)
-        response.raise_for_status()
-    except requests.RequestException:
-        flash('Произошла ошибка при проверке', 'danger')
-
-    url_info = db.extract_page_data(response)
-    db.insert_checks(conn, id, url_info)
-    flash('Страница успешно проверена', 'success')
-    db.close_connection(conn)
-    return redirect(url_for('show_url', id=id))
-
+app.register_blueprint(main_bp)
 
 if __name__ == 'main':
-    index()
+    app.run(debug=True)
---
+++ page_analyzer/blueprints/main.py
@@ -0,0 +1,82 @@
+import requests
+from flask import (
+    Flask,
+    abort,
+    flash,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    Blueprint,
+    current_app,
+)
+
+from page_analyzer import db_manager as db
+from page_analyzer.utils import is_valid, normalize_url
+from page_analyzer.parser import extract_page_data
+
+main_bp = Blueprint('main', __name__)
+
+
+@main_bp.route('/')
+def index():
+    return render_template(
+        'index.html',
+    )
+
+
+@main_bp.route('/urls', methods=['POST'])
+def add_url():
+    url = request.form.to_dict()['url']
+
+    if not is_valid(url):
+        flash('Некорректный URL', 'danger')
+        return render_template(
+            'index.html',
+            url=url,
+        ), 422
+    
+    normalized_url = normalize_url(url)
+    with db.connect_db(current_app) as conn:
+        existed_url = db.get_url_by_name(conn, normalized_url)
+        if existed_url:
+            flash('Страница уже существует', 'warning')
+            url_id = existed_url.id
+        else:
+            url_id = db.insert_url(conn, normalized_url)
+            flash('Страница успешно добавлена', 'success')
+    return redirect(url_for('main.show_url', id=url_id))
+
+
+@main_bp.route('/urls/<int:id>', methods=['GET'])
+def show_url(id: int):
+    with db.connect_db(current_app) as conn:
+        url = db.get_url_by_id(conn, id)
+        checks = db.get_checks(conn, id)
+    if not url:
+        abort(404)
+    return render_template('url.html', url=url, checks=checks)
+
+
+@main_bp.route('/urls', methods=['GET'])
+def show_urls():
+    with db.connect_db(current_app) as conn:
+        urls = db.get_urls(conn)
+    return render_template('urls.html', urls=urls)
+
+
+@main_bp.route('/urls/<int:id>/checks', methods=['POST'])
+def check_url(id: int):
+    with db.connect_db(current_app) as conn:
+        url = db.get_url_by_id(conn, id)
+        if not url:
+            abort(404)
+        try:
+            response = requests.get(url.name)
+            response.raise_for_status()
+            url_info = extract_page_data(response)
+            db.insert_checks(conn, id, url_info)
+            flash('Страница успешно проверена', 'success')
+        except requests.RequestException:
+            flash('Произошла ошибка при проверке', 'danger')
+    return redirect(url_for('main.show_url', id=id))
